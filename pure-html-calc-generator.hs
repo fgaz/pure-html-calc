a3558c69444a2116f6618fbeb3ee9ec6212065a4
Francesco Gazzetta <fgaz@users.noreply.github.com>
2015-01-19 18:18:23 +0100

    mainly comments

--- pure-html-calc-generator.hs
+++ pure-html-calc-generator.hs
@@ -10,12 +10,16 @@ ch2f _ = error "You have to define the functions associated to additional operat
 
 mkdir = createDirectoryIfMissing True
 
+--possible digits/operators. strings because of the frequent use of ++
 nums = map show [0..9]
+ops = ["+","-","*",":"]
+
+--monadic functions to add the next digit/operator
 plusx x str = map (str++) x
 plusnums = plusx nums
 plusops = plusx ops
-ops = ["+","-","*",":"]
 
+--generates the html
 template :: Bool -> Bool -> String -> String
 template enablenums enableops screen = "\
   \<!DOCTYPE html>\
@@ -41,36 +45,42 @@ template enablenums enableops screen = "\
     mkbtn enabled name dest = "<button type=\"submit\" " ++ (btnattr enabled dest) ++ ">" ++ name ++ "</button>"
     mkbtn' enabled x = mkbtn enabled x x
 
-mkresulthtml actions = template False False $ (actions ++ "=" ++ (show $ calcresult actions))
+--generates the html of a result page
+resulthtml actions = template False False $ (actions ++ "=" ++ (show $ calcresult actions))
 
-mkresultfile actions = writeFile ((actions2dir actions) ++ "/index.html") $ mkresulthtml actions
-
+--all the file-making functions
+mkresultfile actions = writeFile ((actions2dir actions) ++ "/index.html") $ resulthtml actions
 mknumselectfile actions = writeFile ((actions2dir actions) ++ "/index.html") $ template True False actions
-
 mkopselectfile  actions = writeFile ((actions2dir actions) ++ "/index.html") $ template False True actions
 
 calcresult str = f a b
   where
-    a = read ((str!!0):(str!!1):[])
-    b = read ((str!!3):(str!!4):[])
-    f = ch2f (str!!2)
+    a = read ((str!!0):(str!!1):[]) --first number
+    b = read ((str!!3):(str!!4):[]) --second number
+    f = ch2f (str!!2) --operator
 
 actions2dir = ("build/"++) . (intersperse '/')
 
 main :: IO ()
 main = do
+  putStrLn "Generating the pure html calculator in build/ ..."
+  
   mkdir "build"
+  --start page
   writeFile "build/index.html" $ template True False ""
-  --let l = nums >>= plusnums >>= plusops >>= plusnums >>= plusnums
+  
+  --the 5 layers of the calculator
   let l0 = nums
   let l1 = l0 >>= plusnums
   let l2 = l1 >>= plusops
   let l3 = l2 >>= plusnums
   let l4 = l3 >>= plusnums
   
+  --first all the folders
   sequence $ map (mkdir . actions2dir) l4
+  --then the result files
   sequence $ map mkresultfile l4
-  
+  --then all the others
   sequence $ map mknumselectfile l0
   sequence $ map mkopselectfile l1
   sequence $ map mknumselectfile l2
